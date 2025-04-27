c16399f38fe53363c434debbfcc5812ec4359e4b
Sallam124 <153922132+Sallam124@users.noreply.github.com>
2025-04-28 02:18:02 +0300

    Bite Garden

--- viva_journal/macos/Flutter/GeneratedPluginRegistrant.swift
+++ viva_journal/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,9 +13,11 @@ import flutter_tts
 import google_sign_in_ios
 import local_auth_darwin
 import path_provider_foundation
+import quill_native_bridge_macos
 import shared_preferences_foundation
 import speech_to_text
 import sqflite_darwin
+import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -27,8 +29,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
